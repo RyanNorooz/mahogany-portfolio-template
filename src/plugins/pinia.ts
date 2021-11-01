f0b8a1d5cc8c36956c4851b1364235ebfdccfed2
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-11-01 06:53:21 +0330

    fix: rename modules folder to plugins

--- src/plugins/pinia.ts
+++ src/plugins/pinia.ts
@@ -1,5 +1,5 @@
 import { createPinia } from 'pinia'
-import { UserModule } from '~/types'
+import { UserModule } from '@/types'
 
 // Setup Pinia
 // https://pinia.esm.dev/
@@ -9,9 +9,6 @@ export const install: UserModule = ({ isClient, initialState, app }) => {
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
   // for other serialization strategies.
-  if (isClient)
-    pinia.state.value = (initialState.pinia) || {}
-
-  else
-    initialState.pinia = pinia.state.value
+  if (isClient) pinia.state.value = initialState.pinia || {}
+  else initialState.pinia = pinia.state.value
 }
