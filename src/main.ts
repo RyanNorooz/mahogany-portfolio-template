1f1c5077330b534ece5d38732bd0f0e427e3ff87
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-11-02 03:13:46 +0330

    style: refactor & modulerize styles from template

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-// register vue composition api globally
+/* eslint-disable import/no-unresolved */
+
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
@@ -7,21 +8,21 @@ import App from './App.vue'
 // windicss layers
 import 'virtual:windi-base.css'
 import 'virtual:windi-components.css'
-// your custom styles here
-import './styles/main.css'
-// windicss utilities should be the last style import
-import 'virtual:windi-utilities.css'
-// windicss devtools support (dev only)
-import 'virtual:windi-devtools'
+import './styles/main.scss'
+import 'virtual:windi-utilities.css' // windicss utilities should be the last style import
+import 'virtual:windi-devtools' // windicss devtools support (dev only)
 
 const routes = setupLayouts(generatedRoutes)
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
+  // ? vite-ssg takes care of mounting the app for us
   App,
   { routes },
   (ctx) => {
     // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
-  },
+    Object.values(import.meta.globEager('./plugins/*.ts')).map((i) =>
+      i.install?.(ctx)
+    )
+  }
 )
---
+++ src/styles/README.md
@@ -0,0 +1,76 @@
+<!-- TODO: add links -->
+
+# Table of Contents
+
+- Project: Nafie - HTML Portfolio Template
+- Version: 2.0
+- Created: 04/2021
+- Updated: 10/2021
+
+1. **Color Codes**
+
+   1. Primary Color
+   2. Secondary Color
+   3. Light Color
+   4. Dark Color
+   5. Success Color
+   6. Danger Color
+   7. Warning Color
+   8. Info Color
+
+   <br>
+
+2. **Fonts Family**
+
+   1. Logo Font
+   2. Headings Font
+   3. Body Font
+
+   <br>
+
+3. **Animations**
+
+   1. Zoom Effects
+   2. Fade Effects
+   3. Spinner Effects
+   4. Scale Effects
+   5. Rotate Effects
+   6. Slide Effects
+   7. Pulse Effects
+   8. LightSpeed Effects
+   9. Flash Effect
+   10. Shine Effects
+   11. Floating Effect
+
+   <br>
+
+4. **Reset & Global elements**
+
+5. **UI Elements Styles**
+
+   1. Forms
+   2. Toggle Switch Button
+   3. Custom Tooltip
+   4. Ultimate Tooltip
+
+   <br>
+
+6. **Components Styles**
+
+   1. Circle Cursor
+   2. Preloader
+   3. Header
+   4. Footer
+   5. Scroll to Top Button
+   6. Ajax Loading
+   7. Toast Notifications
+
+   <br>
+
+7. **Pages Styles**
+
+   1. Homepage
+   2. Single Portfolio
+   3. Not Found (404)
+
+   <br>
