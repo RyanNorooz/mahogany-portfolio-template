29e799d9d8546f8f6bcd8d8e049f47ad457ea661
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-11-03 10:34:09 +0330

    feat: add 404 error page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,14 +1,34 @@
-<script setup lang="ts">
-const { t } = useI18n()
-</script>
-
-<template>
-  <div>
-    {{ t('not-found') }}
-  </div>
-</template>
-
 <route lang="yaml">
 meta:
-  layout: 404
+  layout: error
 </route>
+
+<script lang="ts">
+export default defineComponent({ name: '404Page' })
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <!-- start of 404 page -->
+  <div class="not-found-page page">
+    <div class="not-found-content">
+      <div class="container">
+        <div class="not-found-img">
+          <img src="@/assets/404.png" alt="404" />
+        </div>
+
+        <div class="not-found-text">
+          <h2 class="title">Oops! That page can’t be found</h2>
+
+          <p class="desc">
+            We're sorry, we couldn't find the page you requested.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end of 404 page -->
+</template>
+
+<style lang="scss" scoped></style>
