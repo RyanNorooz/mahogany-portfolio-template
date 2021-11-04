e0afbad8cba612565b0fd65b5462afe1d9336a7d
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-11-04 07:48:43 +0330

    refactor: move html from template to vue pages

---
+++ src/pages/Archive.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+export default defineComponent({ name: 'ProjectArchivePage' })
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <!-- start of archive page -->
+  <div class="archive-page portfolio-section section page">
+    <div class="container">
+      <div class="section-title">
+        <span class="subtitle">{{ portfolioArchiveName }}</span>
+        <h2 class="title">
+          The latest projects in
+          <br />
+          {{ portfolioArchiveName }} taxonomy
+        </h2>
+      </div>
+
+      <div class="section-content">
+        <!-- portfolio items -->
+        <ul
+          ref="portfolioItems"
+          class="portfolio-items"
+          data-no-works-msg="No works to load"
+        >
+          <li
+            v-for="work in filteredPortfolioItems"
+            :key="work.title.en"
+            class="portfolio-item"
+          >
+            <a :href="work.url" target="_blank" rel="noopener">
+              <div class="item-img">
+                <img :src="work.imgUrl" :alt="work.title.en" />
+              </div>
+              <div class="item-details">
+                <h3 class="title">{{ work.title.en }}</h3>
+                <div class="date">{{ work.date.en }}</div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <button class="more" @click="getPortfolioItems">
+              Load more works
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- end of archive page -->
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,71 @@
+<script lang="ts">
+export default defineComponent({ name: 'PortfolioPage' })
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <!-- start of portfolio page -->
+  <div class="portfolio-page portfolio-section section page">
+    <div class="container">
+      <div class="section-content">
+        <!-- portfolio items -->
+        <ul ref="portfolioItems" class="portfolio-items">
+          <li>
+            <!-- text box -->
+            <div class="text-box-block">
+              <span class="subtitle">
+                <a
+                  :href="
+                    'archive-portfolio.html?tax=cat&cat=' +
+                    getSinglePortfolioItem.category
+                  "
+                  >{{ getSinglePortfolioItem.category }}</a
+                >
+              </span>
+              <h2>{{ getSinglePortfolioItem.title.en }}</h2>
+              <p>{{ getSinglePortfolioItem.desc.en }}</p>
+              <ul class="work-tools">
+                <li v-for="tool in getSinglePortfolioItem.tools" :key="tool">
+                  <a :href="'archive-portfolio.html?tax=tools&tools=' + tool">{{
+                    tool
+                  }}</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <!-- start items list -->
+          <li
+            v-for="screenshot in getSinglePortfolioItem.screenshots"
+            :key="screenshot.caption.en"
+            class="portfolio-item"
+          >
+            <!-- href points to the fully sized image -->
+            <a
+              class="glightbox"
+              target="_blank"
+              rel="noopener"
+              data-gallery="gallery1"
+              :href="screenshot.url"
+              :data-glightbox="'title:' + screenshot.caption.en"
+            >
+              <div class="item-img">
+                <!-- thumbnails -->
+                <img :src="screenshot.url" :alt="screenshot.caption.en" />
+              </div>
+              <div class="item-details">
+                <h3 class="title">{{ screenshot.caption.en }}</h3>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- end of portfolio page -->
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
